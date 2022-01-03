068e78b75972814f3e8908da1704cced83047071
Gerardo <gerardo@latteandcode.com>
2022-01-03 12:17:33 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useMemo, useState } from "react";
+import "./App.css";
+
+interface IBeforeInstallPromptEvent extends Event {
+  readonly platforms: string[];
+  readonly userChoice: Promise<{
+    outcome: "accepted" | "dismissed";
+    platform: string;
+  }>;
+  prompt(): Promise<void>;
+}
+
+const isInWebApp =
+  window.navigator.standalone === true ||
+  window.matchMedia("(display-mode: standalone)").matches;
+
+function useAddToHomeScreenPrompt() {
+  const [prompt, setPrompt] = useState<IBeforeInstallPromptEvent | null>(null);
+  const [isReady, setIsReady] = useState(false);
+  const [hasFinishedInstallation, setHasFinishedInstallation] =
+    useState(isInWebApp);
+
+  const finishInstallation = useCallback(function () {
+    setHasFinishedInstallation(true);
+  }, []);
+
+  const promptToInstall = useCallback(
+    function () {
+      if (prompt) {
+        prompt.prompt();
+        prompt.userChoice.then(function (choiceResult) {
+          if (choiceResult.outcome === "accepted") {
+            finishInstallation();
+          }
+        });
+        return;
+      }
+
+      return Promise.reject(
+        new Error(
+          'Tried installing before browser sent "beforeinstallprompt" event'
+        )
+      );
+    },
+    [finishInstallation, prompt]
+  );
+
+  useEffect(
+    function () {
+      if (prompt) {
+        setIsReady(true);
+      }
+    },
+    [prompt]
+  );
+
+  useEffect(function () {
+    function ready(e: IBeforeInstallPromptEvent) {
+      e.preventDefault();
+      setPrompt(e);
+    }
+
+    window.addEventListener("beforeinstallprompt", ready as any);
+
+    return function () {
+      window.removeEventListener("beforeinstallprompt", ready as any);
+    };
+  }, []);
+
+  return useMemo(
+    () => ({
+      isReady,
+      promptToInstall,
+      hasFinishedInstallation,
+      finishInstallation,
+    }),
+    [isReady, promptToInstall, hasFinishedInstallation, finishInstallation]
+  );
+}
 
 function App() {
+  const { isReady, promptToInstall, hasFinishedInstallation } =
+    useAddToHomeScreenPrompt();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>💛 Latte and Code</h1>
+      <p>Instala esta aplicación en tu dispositivo</p>
+      <p>
+        {isReady && !hasFinishedInstallation && (
+          <button onClick={() => promptToInstall()}>Instalar</button>
+        )}
+      </p>
+      <p>
+        {hasFinishedInstallation
+          ? "Estás usando la Web App"
+          : "Aplicación todavía no instalada"}
+      </p>
     </div>
   );
 }
